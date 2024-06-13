9e4c0f5cc18fee00ac33fe19bb4c3b76e794cfd1
DanielSantaz <danielsantaz@gmail.com>
2024-06-13 11:37:44 -0400

    character branch created

---
+++ Character.java
@@ -0,0 +1,18 @@
+public class Character {
+    private String id;
+    private String name;
+    private int hp;
+    private boolean isAlive;
+
+    public Character (String name, int hp){
+        this.name = name;
+        this.isAlive = true;
+
+    }
+
+
+
+
+
+
+}
