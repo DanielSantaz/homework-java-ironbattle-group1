926758a4e933b3e566c0bf149f1b610c1ea65169
DanielSantaz <danielsantaz@gmail.com>
2024-06-13 11:55:26 -0400

    parameters to character

--- Character.java
+++ Character.java
@@ -5,14 +5,42 @@ public class Character {
     private boolean isAlive;
 
     public Character (String name, int hp){
+        this.id = "0";
         this.name = name;
         this.isAlive = true;
-
+        this.hp = hp;
     }
 
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
